a5b7c5f4ae1249c0da4554286108a175d4bfd169
Jhonah <buryakone@gmail.com>
2020-10-06 23:20:29 +0700

    в этот раз картинки меня с помощью изменения src в html, все остальное как в прошлых задачах

--- js/app.js
+++ js/app.js
@@ -6,18 +6,48 @@ const images = [
     "customer-4.jpg"
 ]
 
+const names = [
+    "Oleg",
+    "Leha",
+    "Sanya",
+    "Dima",
+    "Tommy"
+]
+
+const texts = [
+    "Text messages are used for personal, family, business and social purposes. Governmental and non-governmental organizations use text messaging for communication between colleagues.",
+    "Text messages may be sent over a cellular network, or may also be sent via an Internet connection.",
+    "'Txt msg' redirects here. For the TV series, see Pop-Up Video.",
+    "From Wikipedia, the free encyclopedia",
+    "Text messaging"
+]
+
+let count = 0;
+
 const buttons = document.querySelectorAll('.btn');
 const img = document.getElementById('customer-img');
 const name = document.getElementById('customer-name');
 const text = document.getElementById('customer-text');
 
+name.textContent = 'Fedor';
+text.textContent = "Description";
+
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-      if ( button.classList.contains('prevBtn') ) {
-        alert('Left');36000-22500=13500
-      }
-      else if ( button.classList.contains('nextBtn') ) {
-        alert('Right');
-      }
+        if ( button.classList.contains('prevBtn') ) {
+            count--;
+            if (count < 0) {
+                count = images.length - 1;
+            }
+        }
+        else if ( button.classList.contains('nextBtn') ) {
+            count++;
+            if (count > images.length - 1) {
+                count = 0;
+            }
+        }
+        img.src = `img/${images[count]}`;
+        name.textContent = names[count];
+        text.textContent = texts[count];
     })
 })
